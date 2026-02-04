c1ea6f8135ed1450937bfbe41153d0eca6fd9ab0
Anfal <alranfal007@gmail.com>
2026-02-04 13:26:01 +0530

    Initial clean commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>My Portfolio</h1>
+      <p>Building from scratch with react</p>
+    </div>
+  )
+}
+
+export default App
+
